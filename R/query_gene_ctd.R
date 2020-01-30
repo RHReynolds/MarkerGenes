58d8205ce3c7a362f571faced9e59ae7da75db5f
RHReynolds <regina.reynolds.16@ucl.ac.uk>
2020-01-30 15:59:17 +0000

    Updated to reflect median in newer EWCE ctd.

--- R/query_gene_ctd.R
+++ R/query_gene_ctd.R
@@ -9,6 +9,9 @@
 #'   mean expression and specificity matrices from a scRNAseq study.
 #' @param celltypeLevel Either 1 or 2, depending on whether user wants to query
 #'   level 1 or 2 cell types.
+#' @param median_included logical. If from newer version of EWCE ctd may include
+#'   median expression and specificity. Setting to TRUE will include these in
+#'   the final table.
 #' @param genelistSpecies Either 'mouse' or 'human' depending on whether MGI or
 #'   HGNC symbols are used.
 #' @param ctdSpecies Either 'mouse' or 'human' depending on the ctd datasets
@@ -20,22 +23,22 @@
 #'
 
 query_gene_ctd <- function(genes, ... , celltypeLevel = c(1, 2),
+                           median_included = FALSE,
                            genelistSpecies = c("mouse", "human"),
                            ctdSpecies = c("mouse", "human")) {
 
   # Extract names of ctd inputs to name elements of list
   # Need to remove first unnamed argument, which is the function name, and named arguments.
   argument_names <- as.list(match.call()) %>%
-    within(., rm(genes, celltypeLevel, genelistSpecies, ctdSpecies))
+    within(., rm(genes, celltypeLevel, median_included, genelistSpecies, ctdSpecies))
   argument_names <- argument_names[-1] %>%
     as.character()
 
   # Create list
   ctd_list <- setNames(list(...), argument_names)
 
-  # Comment out when exporting package
-
-  source("/home/rreynolds/projects/MarkerGenes/R/conversion_functions.R")
+  # # Comment out when exporting package
+  # source("/home/rreynolds/projects/MarkerGenes/R/conversion_functions.R")
 
   # Check genelist and ctd species are the same. If not convert genes to same species as ctd.
   if (genelistSpecies == "human" & ctdSpecies == "mouse" |
@@ -50,29 +53,56 @@ query_gene_ctd <- function(genes, ... , celltypeLevel = c(1, 2),
     # Filter specificity by gene list
     filtered_specificity <- ctd[[celltypeLevel]]$specificity %>%
       as_tibble(., rownames = "Gene") %>%
-      filter(Gene %in% genes) %>%
-      gather(key = "CellType", value = "Specificity",-Gene)
+      dplyr::filter(Gene %in% genes) %>%
+      tidyr::gather(key = "CellType", value = "Specificity",-Gene)
 
     # Filter mean expression by gene list
     filtered_mean_exp <- ctd[[celltypeLevel]]$mean_exp %>%
       as_tibble(., rownames = "Gene") %>%
-      filter(Gene %in% genes) %>%
-      gather(key = "CellType", value = "Mean_Expression",-Gene)
+      dplyr::filter(Gene %in% genes) %>%
+      tidyr::gather(key = "CellType", value = "Mean_Expression",-Gene)
+
+    if(median_included == TRUE){
+
+      # Filter median specificity by gene list
+      filtered_median_specificity <- ctd[[celltypeLevel]]$median_specificity %>%
+        as_tibble(., rownames = "Gene") %>%
+        dplyr::filter(Gene %in% genes) %>%
+        tidyr::gather(key = "CellType", value = "Median_Specificity",-Gene)
+
+      # Filter median expression by gene list
+      filtered_median_exp <- ctd[[celltypeLevel]]$median_exp %>%
+        as_tibble(., rownames = "Gene") %>%
+        dplyr::filter(Gene %in% genes) %>%
+        tidyr::gather(key = "CellType", value = "Median_Expression",-Gene)
+
+      joint_table <- filtered_specificity %>%
+        dplyr::inner_join(filtered_mean_exp, by = c("Gene", "CellType")) %>%
+        dplyr::inner_join(filtered_median_specificity, by = c("Gene", "CellType")) %>%
+        dplyr::inner_join(filtered_median_exp, by = c("Gene", "CellType")) %>%
+        dplyr::mutate(Study = names(ctd_list[i])) %>%
+        dplyr::mutate(Study_species = ctdSpecies) %>%
+        dplyr::select(Study, Study_species, everything()) %>%
+        dplyr::arrange(Gene, desc(Specificity))
+
+    } else{
 
     # Join both filtered tibbles
     joint_table <- filtered_specificity %>%
-      inner_join(filtered_mean_exp, by = c("Gene", "CellType")) %>%
+      dplyr::inner_join(filtered_mean_exp, by = c("Gene", "CellType")) %>%
       dplyr::mutate(Study = names(ctd_list[i])) %>%
       dplyr::mutate(Study_species = ctdSpecies) %>%
       dplyr::select(Study, Study_species, everything()) %>%
-      arrange(Gene, desc(Specificity))
+      dplyr::arrange(Gene, desc(Specificity))
+
+    }
 
     # Add tibble to list
     if (i == 1) {
       master_df <- joint_table
     } else {
       master_df <- master_df %>%
-        bind_rows(joint_table)
+        dplyr::bind_rows(joint_table)
     }
 
   }
